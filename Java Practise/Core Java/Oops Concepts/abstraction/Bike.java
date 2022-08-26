33722062577939899914797d939feec2e1a08ede
j3gad335h <ntjegadeesh@gmail.com>
2022-08-26 13:11:02 +0530

    Update

---
+++ Java Practise/Core Java/Oops Concepts/abstraction/Bike.java
@@ -0,0 +1,11 @@
+package abstraction;
+
+abstract class Bike {
+	
+	abstract void run();
+	abstract void gear1();
+	abstract void gear2();
+	abstract void gear3();
+	abstract void gear4();
+	abstract void breakApplied();
+}
