15d0bfaa1b809db285bb82d449247ac6eb1705d6
Jegadeesh <101029948+j3gad335h@users.noreply.github.com>
2023-04-24 18:51:20 +0530

    Delete IMDB Web Clone -Node JS directory

--- IMDB Web Clone -Node JS/test.js
+++
@@ -1,3 +0,0 @@
-const test='I am a Hero "Kiddiimg",'
-var test1=test.replaceAll('"','')
-console.log(test1)
--- IMDB Web Clone -Node JS/views/test.py
+++
@@ -1,6 +0,0 @@
-import os
-
-folder="static/uploads"
-
-file='jeg.png'
-file.save(os.path.join(folder),'jeg')
